b477371a25f61b845ae8852c6ab150e7b8556e07
orcasweb <44039158+orcasweb@users.noreply.github.com>
2018-10-18 09:13:14 -0400

    committing stashes

--- example/NumbersAndBooleans.py
+++ example/NumbersAndBooleans.py
@@ -7,3 +7,11 @@ import IntroToStrings
 
 answer = IntroToStrings.name.endswith("Simpson")
 print("The answer is: ", answer)
+print(" Checking for boolean")
+print("bool(0) is: ",bool(0))
+print("bool(1) is: ",bool(1))
+print("bool(100) is: ",bool(100))
+print("bool(-1) is: ",bool(-1))
+
+street = "34th street apple pear banana"
+print("street title: ", street.title())
