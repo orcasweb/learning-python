1e29633c79e795b5084cc55b9d3afcc6bd4c3e0b
orcasweb <44039158+orcasweb@users.noreply.github.com>
2018-10-18 15:58:03 -0400

    added file VariablesLab

---
+++ example/VariablesLab.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 18, 2018
+
+@author: orcasweb
+'''
+
+travel_month = None
+print("travel_month is: ",travel_month)
+travel_month = "january"
+print("travel_month is assigned and  is: ",travel_month)
+number_of_weeks = None
+print("number_of_weeks is: ",number_of_weeks)
+number_of_weeks = 3
+print("number_of_weeks is assigned and it is: ",number_of_weeks)
+number_of_weeks = 5
+print("number_of_weeks is updated and it is: ",number_of_weeks)
