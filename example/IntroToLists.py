58677228f03b52dd9aac57b9ccc52128bf4df371
orcasweb <44039158+orcasweb@users.noreply.github.com>
2018-10-22 09:03:08 -0400

    started intro to lists

--- example/IntroToLists.py
+++ example/IntroToLists.py
@@ -4,3 +4,8 @@ Created on Oct 19, 2018
 @author: orcasweb
 '''
 print("In this lesson we will learn about lists")
+top_travel_cities = ['Solta', 'Greenville', 'Buenos Aires', 'Los Cabos', 'Walla Walla Valley', 'Marakesh', 'Albuquerque', 'Archipelago Sea', 'Iguazu Falls', 'Salina Island', 'Toronto', 'Pyeongchang']
+countries_of_top_cities = ['Croatia', 'USA', 'Argentina', 'Mexico', 'USA', 'Morocco', 'New Mexico', 'Finland', 'Argentina', 'Italy', 'Canada', 'South Korea']
+print(top_travel_cities)
+
+print(top_travel_cities[0])
