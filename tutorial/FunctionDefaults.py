220af5da5118b48fbd83c287bc7714a8dd38afb0
orcasweb <44039158+orcasweb@users.noreply.github.com>
2018-10-25 10:43:13 -0400

    commit

--- tutorial/FunctionDefaults.py
+++ tutorial/FunctionDefaults.py
@@ -18,3 +18,15 @@ def parrot(voltage, state='a stiff', action='voom', mytype='Norwegian Blue'):
     print("-- It's", state, "!")
 
 parrot(mytype="me",action= "play",state="sleep",voltage=10)
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+        
+print("*"*80)
+cheeseshop("venu")        
